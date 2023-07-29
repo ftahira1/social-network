6a4e7dc2a1d6891606b1ad2602f7cc998da854e5
ftahira1 <123195285+ftahira1@users.noreply.github.com>
2023-07-29 09:17:12 -0400

    Created and completed model folder

---
+++ models/reaction.js
@@ -0,0 +1,46 @@
+const { Schema, Types } = require("mongoose");
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+      minlength: 10,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: formatDate,
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// getter to format date upon query
+function formatDate(createdAt) {
+  return createdAt.toLocaleDateString("en-US", {
+    day: "2-digit",
+    year: "numeric",
+    month: "long",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+};
+
+const Reaction = model('Reaction', reactionSchema);
+
+module.exports = Reaction;
